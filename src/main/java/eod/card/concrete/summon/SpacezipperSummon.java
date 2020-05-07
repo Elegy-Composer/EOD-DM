4c5e9809a2b5391445bbf10fc2a451b7e887e2d3
aoaaceai <54458354+aoaaceai@users.noreply.github.com>
2020-05-07 09:36:04 +0800

    Added Spacezipper's summon card

---
+++ src/main/java/eod/card/concrete/summon/SpacezipperSummon.java
@@ -0,0 +1,41 @@
+package eod.card.concrete.summon;
+
+import eod.Party;
+import eod.Player;
+import eod.card.abstraction.Card;
+import eod.card.abstraction.summon.SummonCard;
+import eod.warObject.character.concrete.transparent.Spacezipper;
+
+import java.awt.*;
+
+public class SpacezipperSummon extends SummonCard {
+    public SpacezipperSummon(Player p) {
+        super(p);
+    }
+
+    @Override
+    public void summon() {
+        Point p = player.selectPosition(player.getBase());
+        player.summonObject(new Spacezipper(player, p.x, p.y));
+    }
+
+    @Override
+    public Card copy() {
+        return new SpacezipperSummon(player);
+    }
+
+    @Override
+    public int getCost() {
+        return 7;
+    }
+
+    @Override
+    public String getName() {
+        return "召喚 空間之鏈-圭月";
+    }
+
+    @Override
+    public Party getParty() {
+        return Party.TRANSPARENT;
+    }
+}
