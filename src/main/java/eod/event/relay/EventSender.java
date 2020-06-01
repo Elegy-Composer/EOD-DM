de6058571a8ed26ee8ac4a9642c448e71548299e
aoaaceai <54458354+aoaaceai@users.noreply.github.com>
2020-06-01 10:06:45 +0800

    StatusHolder

--- src/main/java/eod/event/relay/EventSender.java
+++ src/main/java/eod/event/relay/EventSender.java
@@ -8,7 +8,7 @@ public interface EventSender {
 
     void unregisterReceiver(EventReceiver receiver);
 
-    EventReceiver[] getStatusHolders();
+    StatusHolder[] getStatusHolders();
 
     void send(GameObject sender, Event event);
 }
